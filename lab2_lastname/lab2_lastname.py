739a8ef87b06ea6aeab533e01e572b73091df91a
prof Wu <hwu@qcc.cuny.edu>
2025-09-03 11:56:28 -0400

    add hypotenusa calculation

--- lab2_lastname/lab2_lastname.py
+++ lab2_lastname/lab2_lastname.py
@@ -5,3 +5,9 @@ Sep 3, 2025
 """
 username = input("Enter a username: ")
 print(f"Welcome to Git commands {username}")
+
+# calculate the Hypotenusa
+x = 5
+y = 3
+hyp = (x**2 + y**2)**0.5
+print(f"The hypotenusa of sides {x} and {y} is {hyp:0.2f}")
