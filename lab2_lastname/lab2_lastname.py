e718f703a9a0acf53f068ff115f02a06e18386e8
prof Wu <hwu@qcc.cuny.edu>
2025-09-03 10:47:25 -0400

    update lab 2

--- lab2_lastname/lab2_lastname.py
+++ lab2_lastname/lab2_lastname.py
@@ -3,4 +3,5 @@ student's full name
 Lab 2, mastering git workflow
 Sep 3, 2025
 """
-print("Welcome to Git commands")
+username = input("Enter a username: ")
+print(f"Welcome to Git commands {username}")
